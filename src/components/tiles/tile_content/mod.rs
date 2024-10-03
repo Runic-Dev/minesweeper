c762513504604dfa850bb189b5857dda2d2856e9
Runic-Dev <contact.runicdev@gmail.com>
2024-10-03 17:23:55 +0200

    Some style improvements / More responsive

--- src/components/tiles/tile_content/mod.rs
+++ src/components/tiles/tile_content/mod.rs
@@ -4,8 +4,8 @@ pub mod undug_content;
 
 pub fn get_generic_classes() -> Vec<&'static str> {
     vec![
-        "w-5",
-        "h-5",
+        "w-7 sm:w-8 md:w-9 lg:w-10 xl:w-15",
+        "h-7 sm:h-8 md:h-9 lg:h-10 xl:h-15",
         "my-1",
         "rounded",
         "flex",
